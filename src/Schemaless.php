1c27c05f5d619b0d75cb83f3e093883fdec34a97
Sinevia <info@sinevia.com>
2018-07-27 07:36:18 +0100

    Update Schemaless.php

--- src/Schemaless.php
+++ src/Schemaless.php
@@ -4,8 +4,16 @@ namespace Sinevia;
 
 class Schemaless {
 
+    /**
+     * The table name for entities
+     * @var string
+     */
     public static $tableEntity = 'snv_schemaless_entity';
     
+    /**
+     * The table schema for entities
+     * @var array
+     */
     public static $tableEntitySchema = array(
         array("Id", "STRING", "NOT NULL PRIMARY KEY"),
         array("IsActive", "STRING"),
@@ -19,8 +27,16 @@ class Schemaless {
         array("DeletedAt", "STRING"),
     );
     
+    /**
+     * The table name for attributes
+     * @var string
+     */
     public static $tableAttribute = 'snv_schemaless_attribute';
     
+    /**
+     * The table schema for attributes
+     * @var array
+     */
     public static $tableAttributeSchema = array(
         array("Id", "STRING", "NOT NULL PRIMARY KEY"),
         array("EntityId", "STRING", "NOT NULL"),
@@ -41,20 +57,36 @@ class Schemaless {
     }
 
     /**
+     * Returns the database to be used for storage
      * @return \Sinevia\SqlDb
      */
     public static function getDatabase() {
         return db();
     }
 
+    /**
+     * Returns the table used to store entities
+     * @return type
+     */
     public static function getTableEntity() {
         return self::getDatabase()->table(self::$tableEntity);
     }
 
+    /**
+     * Returns the table used to store attributes
+     * @return type
+     */
     public static function getTableAttribute() {
         return self::getDatabase()->table(self::$tableAttribute);
     }
 
+    /**
+     * Creates a new entity and returns it
+     * @param array $entityData
+     * @param array $attributesData
+     * @return array|null
+     * @throws \RuntimeException
+     */
     public static function createEntity($entityData, $attributesData = []) {
         if (isset($entityData['Id']) == false) {
             $entityData['Id'] = \Sinevia\Uid::microUid();
@@ -81,10 +113,15 @@ class Schemaless {
         } catch (\Exception $e) {
             self::getDatabase()->transactionRollBack();
             echo $e->getMessage();
-            return false;
+            return null;
         }
     }
 
+    /**
+     * Retrieves entities
+     * @param array $options
+     * @return array
+     */
     public static function getEntities($options = []) {
         $type = trim($options['Type'] ?? '');
         $query = self::getTableEntity();
@@ -95,6 +132,11 @@ class Schemaless {
         return $entities;
     }
 
+    /**
+     * Retrieves a single entity by ID
+     * @param string $entityId
+     * @return array
+     */
     public static function getEntity($entityId) {
         return self::getTableEntity()
                         ->where('Id', '=', $entityId)
@@ -108,10 +150,16 @@ class Schemaless {
     }
 
     public static function getAttribute($entityId, $key) {
-        return self::getTableAttribute()
-                        ->where('EntityId', '=', $entityId)
-                        ->where('Key', '=', $key)
-                        ->selectOne();
+        $attribute = self::getTableAttribute()
+                ->where('EntityId', '=', $entityId)
+                ->where('Key', '=', $key)
+                ->selectOne();
+
+        if (is_null($attribute)) {
+            return null;
+        }
+
+        return static::attributeValueDecode($attribute['Value']);
     }
 
     public static function setAttribute($entityId, $key, $value) {
@@ -124,7 +172,7 @@ class Schemaless {
                             ->where('EntityId', '=', $entityId)
                             ->where('Key', '=', $key)
                             ->update([
-                                'Value' => $value,
+                                'Value' => static::attributeValueEncode($value),
                                 'UpdatedAt' => date('Y-m-d'),
             ]);
         } else {
@@ -132,11 +180,29 @@ class Schemaless {
                         'Id' => \Sinevia\Uid::microUid(),
                         'EntityId' => $entityId,
                         'Key' => $key,
-                        'Value' => $value,
+                        'Value' => static::attributeValueEncode($value),
                         'CreatedAt' => date('Y-m-d'),
                         'UpdatedAt' => date('Y-m-d'),
             ]);
         }
+    }
+
+    /**
+     * Returns the value
+     * @param type $key
+     * @return string
+     */
+    protected static function attributeValueDecode($value) {
+        return json_decode($value, true);
+    }
+
+    /**
+     * Saves the value
+     * @param object $value
+     * @return boolean
+     */
+    protected static function attributeValueEncode($value) {
+        return json_encode($value);
     }
 
 }
